77a28fc00cddcbe155a1934f450f9fa03886dfa1
Wasiu-hub <contactbow@yahoo.com>
2023-12-11 22:48:50 +0000

    creating a web browser for testing

---
+++ src/test/java/MyFirstSel.java
@@ -0,0 +1,24 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class MyFirstSel {
+
+    // given I am on the next page
+    // when I enter Jeans as a product and click on search button
+    // then I should be able to see jeans as a result title
+
+    @Test
+    public void validateSearchJeans(){
+
+        String expectedResult = "Jeans";
+        String actualResult;
+
+        WebDriverManager.chromedriver().setup(); // configures the browser
+        new ChromeDriver();
+
+
+
+    }
+}
